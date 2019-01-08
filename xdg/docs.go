04cc9145cddaf2a304781e3838fc4c213fbbfc13
Nick Wells <wells.nicholas@gmail.com>
2019-01-08 16:13:27 +0000

    add package docs to xdg

---
+++ xdg/docs.go
@@ -0,0 +1,9 @@
+/*
+
+Package xdg is an implementation of the XDG Base Directory
+Specification. There are functions provided which will return the specified
+values for the various directories that the spec defines. See the
+specification for more details.
+
+*/
+package xdg
