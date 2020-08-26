0f91679ac4fe620fe641c4143640eca9ddca32a4
Nick Wells <wells.nicholas@gmail.com>
2020-08-27 00:21:04 +0100

    Support non-Linux systems - where possible

--- xdg/env.go
+++ xdg/env.go
@@ -2,82 +2,15 @@ package xdg
 
 import (
 	"os"
-	"strings"
 )
 
-// The values given here represent the environment variables and default
-// values from the XDG Base Directory Specification. See that spec for more
-// details about how to use these values.
-const (
-	DataHomeEnvVar     = "XDG_DATA_HOME"
-	DataHomeEnvVarDflt = "$HOME/.local/share"
-
-	ConfigHomeEnvVar     = "XDG_CONFIG_HOME"
-	ConfigHomeEnvVarDflt = "$HOME/.config"
-
-	DataDirsEnvVar     = "XDG_DATA_DIRS"
-	DataDirsEnvVarDflt = "/usr/local/share/:/usr/share/"
-
-	ConfigDirsEnvVar     = "XDG_CONFIG_DIRS"
-	ConfigDirsEnvVarDflt = "/etc/xdg"
-
-	CacheHomeEnvVar     = "XDG_CACHE_HOME"
-	CacheHomeEnvVarDflt = "$HOME/.cache"
-
-	ListSep = ":"
-)
-
-// DataHome returns the value of the XDG_DATA_HOME environment variable or
-// the default value
-func DataHome() string {
-	rval := os.ExpandEnv("$" + DataHomeEnvVar)
-	if rval == "" {
-		rval = os.ExpandEnv(DataHomeEnvVarDflt)
-	}
-
-	return rval
-}
-
-// ConfigHome returns the value of the XDG_CONFIG_HOME environment variable or
-// the default value
+// ConfigHome now uses the standard library function os.UserConfigDir to
+// provide the value. It returns the empty string if that function returns a
+// non-nil error.
 func ConfigHome() string {
-	rval := os.ExpandEnv("$" + ConfigHomeEnvVar)
-	if rval == "" {
-		rval = os.ExpandEnv(ConfigHomeEnvVarDflt)
+	dir, err := os.UserConfigDir()
+	if err != nil {
+		return ""
 	}
-
-	return rval
-}
-
-// DataDirs returns the values in the XDG_DATA_DIRS environment variable or
-// the default values
-func DataDirs() []string {
-	val := os.ExpandEnv("$" + DataDirsEnvVar)
-	if val == "" {
-		val = DataDirsEnvVarDflt
-	}
-
-	return strings.Split(val, ListSep)
-}
-
-// ConfigDirs returns the values in the XDG_CONFIG_DIRS environment variable or
-// the default values
-func ConfigDirs() []string {
-	val := os.ExpandEnv("$" + ConfigDirsEnvVar)
-	if val == "" {
-		val = ConfigDirsEnvVarDflt
-	}
-
-	return strings.Split(val, ListSep)
-}
-
-// CacheHome returns the value of the XDG_CACHE_HOME environment variable or
-// the default value
-func CacheHome() string {
-	rval := os.ExpandEnv("$" + CacheHomeEnvVar)
-	if rval == "" {
-		rval = os.ExpandEnv(CacheHomeEnvVarDflt)
-	}
-
-	return rval
+	return dir
 }
---
+++ xdg/envNonLinux.go
@@ -0,0 +1,23 @@
+// +build !linux
+
+package xdg
+
+// DataHome returns an empty string for non-Linux systems
+func DataHome() string {
+	return ""
+}
+
+// DataDirs returns an empty slice for non-Linux systems
+func DataDirs() []string {
+	return []string{}
+}
+
+// ConfigDirs returns an empty slice for non-Linux systems
+func ConfigDirs() []string {
+	return []string{}
+}
+
+// CacheHome returns an empty string for non-Linux systems
+func CacheHome() string {
+	return ""
+}
