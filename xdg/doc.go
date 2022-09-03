04bcf260e6b967635c8328f77bbc5cc1c341ad60
Nick Wells <wells.nicholas@gmail.com>
2022-09-03 10:35:04 +0100

    refactor: minor changes to keep gofumpt quiet

--- xdg/doc.go
+++ xdg/doc.go
@@ -1,9 +1,7 @@
 /*
-
 Package xdg is an implementation of the XDG Base Directory
 Specification. There are functions provided which will return the specified
 values for the various directories that the spec defines. See the
 specification for more details.
-
 */
 package xdg
